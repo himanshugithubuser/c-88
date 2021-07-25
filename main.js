18a686f6d117ef47cca2324e22b61c23987dfe0d
himanshugithubuser <81744263+himanshugithubuser@users.noreply.github.com>
2021-07-25 15:06:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,165 @@
+var canvas = new fabric.Canvas("myCanvas");
+
+player_x = 10;
+player_y = 10;
+
+block_image_width = 30;
+block_image_height = 30;
+
+var player_object = "";
+var block_image_object = "";
+
+function p_update() {
+    fabric.Image.fromURL("player.png", function(Img) {
+    player_object = Img;
+
+    player_object.scaleToWidth(150);
+    player_object.scaleToHeight(140);
+    player_object.set({
+    top:player_y,
+    left:player_x
+    });
+    canvas.add(player_object);
+});
+}
+
+function new_image(get_image) {
+    fabric.Image.fromURL(get_image, function(Img) {
+    block_image_object = Img;
+
+    block_image_object.scaleToWidth(block_image_width);
+    block_image_object.scaleToHeight(block_image_height);
+    block_image_object.set({
+    top:player_y,
+    left:player_x
+    });
+    canvas.add(block_image_object);
+});
+}
+
+window.addEventListener("keydown", my_keydown);
+function my_keydown(e) {
+    keypressed = e.keyCode;
+    console.log(keypressed);
+
+    if(e.shiftKey == true && keypressed == "80") {
+        console.log("Shift and P key is pressed");
+        block_image_height = block_image_height + 10;
+        block_image_width = block_image_width + 10;
+        document.getElementById("current_width").innerHTML=block_image_width;
+        document.getElementById("current_height").innerHTML=block_image_height;
+    }
+
+    if(e.shiftKey == true && keypressed == "77") {
+        console.log("Shift and M key is pressed");
+        block_image_height = block_image_height - 10;
+        block_image_width = block_image_width - 10;
+        document.getElementById("current_width").innerHTML=block_image_width;
+        document.getElementById("current_height").innerHTML=block_image_height;
+    }
+
+    if(keypressed == "38"){
+        up();
+        console.log("Up arrow key is pressed");
+    }
+
+    if(keypressed == "40"){
+        down();
+        console.log("Down arrow key is pressed");
+    }
+
+    if(keypressed == "37"){
+        left();
+        console.log("Left arrow key is pressed");
+    }
+
+    if(keypressed == "39"){
+        right();
+        console.log("Right arrow key is pressed");
+    }
+
+    if(keypressed == "87"){
+        new_image("wall.jpg");
+        console.log("w key is pressed");
+    }
+
+    if(keypressed == "71"){
+        new_image("ground.png");
+        console.log("g key is pressed");
+    }
+
+    if(keypressed == "68"){
+        new_image("dark_green.png");
+        console.log("d key is pressed");
+    }
+
+    if(keypressed == "76"){
+        new_image("light_green.png");
+        console.log("l key is pressed");
+    }
+
+    if(keypressed == "84"){
+        new_image("trunk.jpg");
+        console.log("t key is pressed");
+    }
+
+    if(keypressed == "82"){
+        new_image("roof.jpg");
+        console.log("r key is pressed");
+    }
+
+    if(keypressed == "85"){
+        new_image("unique.png");
+        console.log("u key is pressed");
+    }
+
+    if(keypressed == "67"){
+        new_image("cloud.jpg");
+        console.log("c key is pressed");
+    }
+
+    if(keypressed == "89"){
+        new_image("yellow_wall.png");
+        console.log("y key is pressed");
+    }
+}
+
+function up() {
+    if(player_y >=0) {
+        player_y = player_y - block_image_height;
+        console.log("block image height =" + block_image_height);
+        console.log("When up is pressed, X = "+ player_x +"|y = "+ player_y);
+        canvas.remove(player_object);
+        p_update();
+    }
+}
+
+function down() {
+    if(player_y <500) {
+        player_y = player_y + block_image_height;
+        console.log("block image height =" + block_image_height);
+        console.log("When down is pressed, X = "+ player_x +"|y = "+ player_y);
+        canvas.remove(player_object);
+        p_update();
+    }
+}
+
+function left() {
+    if(player_x >0) {
+        player_x = player_x - block_image_width;
+        console.log("block image width =" + block_image_width);
+        console.log("When left is pressed, X = "+ player_x +"|y = "+ player_y);
+        canvas.remove(player_object);
+        p_update();
+    }
+}
+
+function right() {
+    if(player_x <=850) {
+        player_x = player_x + block_image_width;
+        console.log("block image width =" + block_image_width);
+        console.log("When right is pressed, X = "+ player_x +"|y = "+ player_y);
+        canvas.remove(player_object);
+        p_update();
+    }
+}
